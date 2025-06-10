23708610f864dcfb59b64d86b84e8788f1420294
c0a241202 <c0a241202a@edu.teu.ac.jp>
2025-06-10 17:18:48 +0900

    kadai

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -4,7 +4,6 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
@@ -13,20 +12,44 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)  # 背景画像を反転
     kk_img = pg.image.load("fig/3.png")  # こうかとん画像のSurface
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+                
 
-        x = tmr%3200
-        screen.blit(bg_img, [-x, 0])  # 1枚目
-        screen.blit(bg_img2, [-x+1600, 0])  # 2枚目
-        screen.blit(bg_img, [-x+3200, 0])  # 3枚目
-        screen.blit(kk_img, [300, 200])
+        key_lst = pg.key.get_pressed()
+        dx = 0
+        dy = 0
+
+        if key_lst[pg.K_UP]:
+            dy -= 1
+        if key_lst[pg.K_DOWN]:
+            dy += 1
+        if key_lst[pg.K_LEFT]:
+            dx -= 1
+        if key_lst[pg.K_RIGHT]:
+            dx += 1
+
+        if not any((dx, dy)):
+            dx = -1
+        elif dx == 0:
+            dx = -1
+
+        kk_rct.move_ip(dx, dy)
+
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x + 1600, 0])
+        screen.blit(bg_img, [-x + 3200, 0])
+        screen.blit(kk_img, kk_rct)
+
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(200)
-
 
 if __name__ == "__main__":
     pg.init()
